874f84738d962ddb83a4864d72c70d13c7939f6a
Lucas Luo <98566376+LWZsama@users.noreply.github.com>
2023-11-14 12:40:06 +0000

    Update nim.py

--- nim.py
+++ nim.py
@@ -3,13 +3,13 @@ import random
 def user_move(coins):
     while True:
         try:
-            user_input = int(input("\n请输入您要拿的硬币数量 (1或2)："))
+            user_input = int(input("\nPlease enter the number of coins you want to take (1 or 2): "))
             if user_input in [1, 2] and user_input <= coins:
                 return user_input
             else:
-                print("无效的输入，请重新输入。")
+                print("Invalid input, please try again.")
         except ValueError:
-            print("无效的输入，请重新输入。")
+            print("Invalid input, please try again.")
 
 def robot_move(coins):
     if coins % 3 == 0:
@@ -22,47 +22,47 @@ def play_game():
 
     while True:
         try:
-            user_input = int(input("请选择您是先手(1)还是后手(2)："))
+            user_input = int(input("Choose if you want to go first (1) or second (2): "))
             if user_input in [1, 2]:
                 if user_input == 1:
                     coins = random.randint(10, 30)
                     if coins % 3 != 0:
                         coins = (coins//3)*3
-                    print(f"您选择先手，初始硬币数量: {coins}")
+                    print(f"You chose to go first, initial number of coins: {coins}")
                 else:
                     coins = random.randint(10, 30)
                     if coins % 3 == 0:
                         coins = coins - random.randint(1, 2)
-                    print(f"您选择后手，初始硬币数量: {coins}")
+                    print(f"You chose to go second, initial number of coins: {coins}")
                     robot_coins = robot_move(coins)
                     coins -= robot_coins
-                    print(f"机器人拿走了 {robot_coins} 个硬币，剩余 {coins} 个硬币。")
+                    print(f"The robot took {robot_coins} coins, {coins} remaining.")
                 break
             else:
-                print("无效的输入，请重新输入。")
+                print("Invalid input, please try again.")
         except ValueError:
-            print("无效的输入，请重新输入。")
+            print("Invalid input, please try again.")
 
     while coins > 0:
         user_coins = user_move(coins)
         coins -= user_coins
-        print(f"您拿走了 {user_coins} 个硬币，剩余 {coins} 个硬币。")
+        print(f"You took {user_coins} coins, {coins} remaining.")
 
         if coins == 0:
-            print("恭喜，您赢了！")
+            print("Congratulations, you won!")
             break
 
         robot_coins = robot_move(coins)
         coins -= robot_coins
-        print(f"机器人拿走了 {robot_coins} 个硬币，剩余 {coins} 个硬币。")
+        print(f"The robot took {robot_coins} coins, {coins} remaining.")
 
         if coins == 0:
-            print("机器人赢了！")
+            print("The robot won!")
             break
 
 if __name__ == "__main__":
     while True:
         play_game()
-        play_again = input("\n要再来一局吗？(y/n)：")
+        play_again = input("\nDo you want to play another round? (y/n): ")
         if play_again.lower() != 'y':
             break
